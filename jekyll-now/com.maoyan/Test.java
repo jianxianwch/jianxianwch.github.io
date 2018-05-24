13cbbac1a05e3ddb1264798946a13ce519dfa4ae
wangchaohui <jianxianwch@126.com>
2018-05-25 00:40:33 +0800

    测试

---
+++ jekyll-now/com.maoyan/Test.java
@@ -0,0 +1,14 @@
+/**
+ * @Auther: wangchaohui02
+ * @Date: 2018/5/25 00:32
+ * @Description:
+ * @Version: 1.0
+ */
+public class Test {
+
+    public static void main(String[] args){
+
+        System.out.println("helloWorld!");
+
+    }
+}
